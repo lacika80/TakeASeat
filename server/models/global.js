9c63f545a3b124abaa99d82811da8972f1d0f5c6
lacika80 <gyurkovics95@gmail.com>
2023-02-01 21:32:03 +0100

    added globals, opening time to db

---
+++ server/models/global.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const globalSchema = mongoose.Schema({
+    is_hide_unavailable: { type: Boolean, default: true },
+    stay_time: {
+        type: Number,
+        default: 90,
+    },
+    free_time: { type: Number, default: 0 },
+    
+});
+
+export default mongoose.model("Global", globalSchema);
