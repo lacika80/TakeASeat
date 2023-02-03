102cd57c2a18e06bd9721ac6af36da18f201f977
lacika80 <gyurkovics95@gmail.com>
2023-02-03 21:44:48 +0100

    fixed the R permissions

--- server/models/restPermission.js
+++ server/models/restPermission.js
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
-import mongooseLong from "mongoose-long";
-mongooseLong(mongoose);
-const {
-    Types: { Long },
-} = mongoose;
+
 
 const restPermissionSchema = mongoose.Schema({
-    permission: { type: Long, required: true },
+    permission: { type: Number, required: true },
     user_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
