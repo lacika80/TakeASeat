204619b3ee03d671df4d6cdb492c07dc095aa3c5
lacika80 <gyurkovics95@gmail.com>
2023-02-10 22:51:44 +0100

    Reworked the restaurant model, not finished

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -1,46 +1,117 @@
 import mongoose from "mongoose";
-
-const restaurantSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    users: [
-        {
+/* active: {
+        tables: [
+            {
+                id: 1121,
+                name: "beltér",
+                tables: [
+                    [
+                        { id: 1, name: "asztal 1/1", posx: 1, posy: 1, seats: 6, space: 1121 },
+                        { id: 2, name: "asztal 1/2", posx: 1, posy: 2, seats: 3, space: 1121 },
+                        { id: 3, name: "asztal 1/3", posx: 1, posy: 3, seats: 4, space: 1121 },
+                    ],
+                    [
+                        { id: 4, name: "asztal 2/1", posx: 2, posy: 1, seats: 2, space: 1121 },
+                        { id: 5, name: "asztal 2/2", posx: 2, posy: 2, seats: 1, space: 1121 },
+                    ],
+                    [{ id: 6, name: "asztal 3/1", posx: 3, posy: 1, seats: 1, space: 1121 }],
+                ],
+            },
+        ],
+        tableOpts: ["kint", "bent", "ablaknál", "dohányzó", "nem dohányzó"],
+    }, */
+const restaurantSchema = mongoose.Schema(
+    {
+        spaces: [
+            {
+                _id: { type: mongoose.ObjectId, default: new mongoose.Types.ObjectId() },
+                name: String,
+                tables: [
+                    {
+                        _id: { type: mongoose.ObjectId, default: new mongoose.Types.ObjectId() },
+                        name: { type: String, required: true },
+                        posx: { type: Number, required: true },
+                        posy: { type: Number, required: true },
+                        tableOpts: [String],
+                    },
+                    {
+                        virtuals: {
+                            space: {
+                                get() {
+                                    return "find out how to get the space which this table inside";
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+        name: { type: String, required: true },
+        owner: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
+            required: true,
         },
-    ],
-    is_active: { type: Boolean, default: 1 },
-    creation_date: {
-        type: Date,
-        default: new Date(),
+        users: [
+            {
+                user: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "User",
+                },
+                permission: { type: Number, required: true },
+                global: {
+                    stay_time: {
+                        type: Number,
+                        default: 90,
+                    },
+                    free_time: { type: Number, default: 0 },
+                },
+            },
+        ],
+        is_active: { type: Boolean, default: 1, alias: "isActive" },
+        permissions: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "RestPermission",
+            },
+        ],
+        ooh: [
+            {
+                day_of_week: { type: Number, required: true, min: 1, max: 7 },
+                open: Date,
+                close: Date,
+                is_open: { type: Boolean, required: true },
+            },
+            { alias: "originalOpeningHours" },
+        ],
+        soh: [
+            {
+                date: Date,
+                open: Date,
+                close: Date,
+            },
+            { alias: "specialOpeningHours" },
+        ],
+        global: {
+            is_hide_unavailable: { type: Boolean, default: true },
+            stay_time: {
+                type: Number,
+                default: 90,
+            },
+            free_time: { type: Number, default: 0 },
+            basicPerm: { type: Number, default: 0 },
+        },
     },
-    permissions: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "RestPermission",
+    {
+        timestamps: true,
+        virtuals: {
+            tableOpts: {
+                get() {
+                    return "find out how to get that list...";
+                },
+            },
         },
-    ],
-    spaces: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Space",
-        },
-    ],
-    openingTimes: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "openingTime",
-        },
-    ],
-    global: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Global",
-        required: true,
-    },
-});
+    }
+);
 
 export default mongoose.model("Restaurant", restaurantSchema);
