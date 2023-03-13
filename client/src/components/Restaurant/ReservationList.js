996aacdac56f7ea8121409bc6f6342cccad1ea22
lacika80 <gyurkovics95@gmail.com>
2023-03-13 23:47:32 +0100

    trying with data display and manipulation

--- client/src/components/Restaurant/ReservationList.js
+++ client/src/components/Restaurant/ReservationList.js
@@ -15,9 +15,11 @@ import {
     DialogContentText,
     Typography,
     Tooltip,
+    Autocomplete,
 } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import { DesktopDatePicker } from "@mui/x-date-pickers";
+import { Stack } from "@mui/system";
+import { DesktopDatePicker, TimePicker } from "@mui/x-date-pickers";
 import { unwrapResult } from "@reduxjs/toolkit";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
@@ -26,12 +28,16 @@ import { useParams } from "react-router-dom";
 import { getDetailedReservations } from "../../features/restaurantsSlice";
 
 const ReservationList = () => {
+    const addResInitialState = { name: "", phone: "", email: "", arrive: moment(), leave: moment(), adult: 0, child: 0, comment: "", tableReqs: [], tableIds: [], creator: {} };
     const rest = useSelector((state) => state.restaurants);
     const [date, setDate] = useState(moment());
     const [isSelected, setIsSelected] = useState(false);
-    const [selected, setSelected] = useState("");
+    const [selected, setSelected] = useState(addResInitialState);
+    const [isEditing, setIsEditing] = useState(false);
     const dispatch = useDispatch();
     const { restId } = useParams();
+    const handleArriveDateChange = (val) => setSelected({ ...selected, arrive: val });
+    const handleLeaveDateChange = (val) => setSelected({ ...selected, leave: val });
 
     useEffect(() => {
         if (rest.active && !(rest.status?.reservations != "loading" || rest.status?.reservations != "succeeded")) dispatch(getDetailedReservations(restId));
@@ -42,9 +48,14 @@ const ReservationList = () => {
         console.log(moment(value));
         setDate(moment(value));
     };
-    const handleSelect = (event, resId) => {
-        setSelected(resId);
+    const handleSelect = (event, res) => {
+        setSelected({ ...res });
         setIsSelected(true);
+    };
+    const handleChange = (e) => {
+        if (!((e.target.name == "adult" || e.target.name == "child") && e.target.value < 0)) {
+            setSelected({ ...selected, [e.target.name]: e.target.value });
+        }
     };
     const tableNames = (tables) => {
         let tablestring = "";
@@ -65,6 +76,18 @@ const ReservationList = () => {
             }
         });
         return tablestring;
+    };
+    const tables = () => {
+        const tables = [];
+        rest.active.spaces.forEach((space) => {
+            space.tables.forEach((row) => {
+                row.forEach((table) => {
+                    console.log(table);
+                    tables.push(table.name);
+                });
+            });
+        });
+        return tables;
     };
     return (
         <>
@@ -116,7 +139,7 @@ const ReservationList = () => {
                                             if (a.arrive < b.arrive) return -1;
                                         })
                                         .map((res) => (
-                                            <TableRow key={res._id} hover onClick={(event) => handleSelect(event, res._id)}>
+                                            <TableRow key={res._id} hover onClick={(event) => handleSelect(event, res)}>
                                                 <TableCell>
                                                     {res.name}{" "}
                                                     {res.comment && (
@@ -126,8 +149,8 @@ const ReservationList = () => {
                                                     )}
                                                 </TableCell>
                                                 <TableCell>{res.child == 0 ? res.adult : `${res.adult}+${res.child}`}</TableCell>
-                                                <TableCell>{moment(res.arrive).format("MM/DD/YYYY hh:mm")}</TableCell>
-                                                <TableCell>{moment(res.leave).format("MM/DD/YYYY hh:mm")}</TableCell>
+                                                <TableCell>{moment(res.arrive).format("YYYY/MM/DD hh:mm")}</TableCell>
+                                                <TableCell>{moment(res.leave).format("YYYY/MM/DD hh:mm")}</TableCell>
                                                 <TableCell>{tableNames(res.tableIds)}</TableCell>
                                                 <TableCell>{tableReqs(res.tableReqs)}</TableCell>
                                             </TableRow>
@@ -151,14 +174,232 @@ const ReservationList = () => {
                     </Table>
                 </Paper>
             </Container>
-            <Dialog open={isSelected} onClose={() => setIsSelected(false)}>
-                <DialogTitle>res name (arrive)</DialogTitle>
+            <Dialog
+                open={isSelected}
+                onClose={() => {
+                    setIsSelected(false);
+                    setIsEditing(false);
+                }}
+            >
+                <DialogTitle>{isSelected && `${selected.name} ${moment(selected.arrive).format("HH:mm")}`}</DialogTitle>
                 <DialogContent>
-                    <DialogContentText>{selected}</DialogContentText>
+                    <DialogContentText sx={{ maxHeight: "50vh", minWidth: "20rem" }}>
+                        {isSelected && isEditing ? (
+                            <Stack spacing={2}>
+                                <TextField
+                                    fullWidth
+                                    id="name"
+                                    type="text"
+                                    label="Név"
+                                    variant="standard"
+                                    name="name"
+                                    value={selected.name == "" ? " " : selected.name}
+                                    onChange={handleChange}
+                                    autoComplete="off"
+                                />
+
+                                <TextField
+                                    fullWidth
+                                    type="tel"
+                                    id="phone"
+                                    name="phone"
+                                    variant="standard"
+                                    label="Telefonszám"
+                                    value={selected.phone == "" ? " " : selected.phone}
+                                    onChange={handleChange}
+                                    autoComplete="off"
+                                />
+
+                                <TextField type="number" id="adult" name="adult" variant="standard" label="Felnőttek száma" value={selected.adult} onChange={handleChange} autoComplete="off" />
+
+                                <TextField type="number" id="child" name="child" variant="standard" label="Gyerekek/babák száma" value={selected.child} onChange={handleChange} autoComplete="off" />
+
+                                <DesktopDatePicker
+                                    label="Érkezés"
+                                    name="arriveDate"
+                                    inputFormat="YYYY-MM-DD"
+                                    onChange={handleArriveDateChange}
+                                    value={selected.arrive}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+
+                                <TimePicker
+                                    label="Érkezés"
+                                    ampm={false}
+                                    name="arriveTime"
+                                    value={selected.arrive}
+                                    onChange={handleArriveDateChange}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+
+                                <DesktopDatePicker
+                                    label="Távozás"
+                                    name="leaveDate"
+                                    inputFormat="YYYY-MM-DD"
+                                    onChange={handleLeaveDateChange}
+                                    value={selected.leave}
+                                    minDate={selected.arrive}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+
+                                <TimePicker
+                                    label="távozás"
+                                    name="leaveTime"
+                                    value={selected.leave}
+                                    /* minTime={moment("10:00", "HH:mm")}
+                        maxTime={moment("22:00", "HH:mm")} */
+                                    ampm={false}
+                                    minDate={selected.arrive}
+                                    /* shouldDisableTime={shouldDisableTime} */
+                                    onChange={handleLeaveDateChange}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+
+                                <Autocomplete
+                                    multiple
+                                    id="table-req"
+                                    options={rest.active.tableOpts}
+                                    onChange={(event, newInputValue) => {
+                                        console.log(event);
+                                        setSelected({ ...selected, tableReqs: newInputValue });
+                                    }}
+                                    defaultValue={selected.tableReqs}
+                                    disableCloseOnSelect
+                                    getOptionLabel={(option) => option}
+                                    renderInput={(params) => <TextField {...params} variant="standard" label="Hely igények" />}
+                                />
+                                {/* <Autocomplete
+                                    multiple
+                                    id="table"
+                                    options={tables}
+                                    defaultValue={selected.tableIds.map((table) => table.name)}
+                                    getOptionLabel={(option) => option}
+                                    onChange={(event, newInputValue) => {
+                                        console.log(event);
+                                        setSelected({ ...selected, tableIds: { name: newInputValue } });
+                                    }}
+                                    renderInput={(params) => <TextField {...params} variant="standard" label="Asztal" />}
+                                /> */}
+                                <TextField
+                                    fullWidth
+                                    multiline
+                                    rows={3}
+                                    id="comment"
+                                    type="text"
+                                    label="Megjegyzés"
+                                    variant="standard"
+                                    name="comment"
+                                    value={selected.comment}
+                                    onChange={handleChange}
+                                    autoComplete="off"
+                                />
+                                {/* <Typography>Létrehozta: {selected.creator.name}</Typography> */}
+                            </Stack>
+                        ) : (
+                            <Stack spacing={2}>
+                                <TextField fullWidth disabled id="name" type="text" label="Név" variant="standard" name="name" value={selected.name == "" ? " " : selected.name} autoComplete="off" />
+                                <TextField
+                                    fullWidth
+                                    disabled
+                                    type="tel"
+                                    id="phone"
+                                    name="phone"
+                                    variant="standard"
+                                    label="Telefonszám"
+                                    value={selected.phone == "" ? " " : selected.phone}
+                                    autoComplete="off"
+                                />
+                                <TextField disabled type="number" id="adult" name="adult" variant="standard" label="Felnőttek száma" value={selected.adult} autoComplete="off" />
+                                <TextField disabled type="number" id="child" name="child" variant="standard" label="Gyerekek/babák száma" value={selected.child} autoComplete="off" />
+                                <TextField
+                                    fullWidth
+                                    disabled
+                                    id="arrive"
+                                    type="text"
+                                    label="Érkezés"
+                                    variant="standard"
+                                    name="arrive"
+                                    value={moment(selected.arrive).format("YYYY/MM/DD HH:mm")}
+                                    autoComplete="off"
+                                />
+                                <TextField
+                                    fullWidth
+                                    disabled
+                                    id="leave"
+                                    type="text"
+                                    label="Távozás"
+                                    variant="standard"
+                                    name="leave"
+                                    value={moment(selected.leave).format("YYYY/MM/DD HH:mm")}
+                                    autoComplete="off"
+                                />{" "}
+                                <Autocomplete
+                                    disabled
+                                    multiple
+                                    id="tags-standard"
+                                    options={selected.tableReqs}
+                                    defaultValue={selected.tableReqs}
+                                    getOptionLabel={(option) => option}
+                                    renderInput={(params) => <TextField {...params} variant="standard" label="Hely igények" />}
+                                />
+                                <Autocomplete
+                                    disabled
+                                    multiple
+                                    id="tags-standard"
+                                    options={[]}
+                                    defaultValue={selected.tableIds.map((item) => item.name)}
+                                    getOptionLabel={(option) => option}
+                                    renderInput={(params) => <TextField {...params} variant="standard" label="Asztal" />}
+                                />
+                                <TextField
+                                    disabled
+                                    fullWidth
+                                    multiline
+                                    rows={3}
+                                    id="comment"
+                                    type="text"
+                                    label="Megjegyzés"
+                                    variant="standard"
+                                    name="comment"
+                                    value={selected.comment}
+                                    autoComplete="off"
+                                />
+                                {/* <Typography>Létrehozta: {selected.creator.name}</Typography> */}
+                            </Stack>
+                        )}
+                    </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={() => setIsSelected(false)}>Mégsem</Button>
-                    <Button onClick={() => {}}>Elfogad</Button>
+                    {!isEditing && <Button onClick={() => setIsEditing(true)}>Szerkesztés</Button>}
+                    {!isEditing && (
+                        <Button
+                            onClick={() => {
+                                setIsSelected(false);
+                                setIsEditing(false);
+                            }}
+                        >
+                            Bezárás
+                        </Button>
+                    )}
+                    {isEditing && (
+                        <Button
+                            onClick={() => {
+                                setIsSelected(false);
+                                setIsEditing(false);
+                            }}
+                        >
+                            Mégsem
+                        </Button>
+                    )}
+                    {isEditing && (
+                        <Button
+                            onClick={() => {
+                                console.log(tables());
+                            }}
+                        >
+                            Elfogad
+                        </Button>
+                    )}
                 </DialogActions>
             </Dialog>
         </>
